53215db66fb3c382050745ea8a3ca0cf370554c5
Skills4Up {Code} <code@skills4up.com>
2025-05-20 21:43:51 -0500

    feat(app): adding more updates for modules

--- sesion07-angular-template-directive-pipe/src/app/custom/custom-routing.module.ts
+++ sesion07-angular-template-directive-pipe/src/app/custom/custom-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomDirectivesExamplesComponent } from './custom-directives-examples.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
